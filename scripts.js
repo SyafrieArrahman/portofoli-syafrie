cecb7f4dfd4ac72ab8ef6ca0cc1fd8c2e4d442ed
Muhammad Syafrie <42405752+SyafrieArrahman@users.noreply.github.com>
2024-08-27 18:21:41 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const skillsElement = document.getElementById("skills");
+const showSkillBtn = document.getElementById("show-skill-btn");
+
+showSkillBtn.addEventListener("click", function() {
+  if (skillsElement.classList.contains("skills-hidden")) {
+    skillsElement.classList.replace("skills-hidden", "skills-visible");
+    this.textContent = "Hide Skill";
+  } else {
+    skillsElement.classList.replace("skills-visible", "skills-hidden");
+    this.textContent = "Show Skill";
+  }
+});
